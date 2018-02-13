ee3751c89157fb60c00c5800b8d767fabbbc2efc
Admin <F:\The Bat!\Data\Mail>
2018-02-13 17:38:48 +0300

    even game

---
+++ src/bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { even } from '../index'; // Импортируем функцию even из index.js
+
+even();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// @flow
 
 import readlineSync from 'readline-sync'; // Импортируем readline-sync
 
@@ -7,4 +6,42 @@ const greeting = () => {
   return console.log(`Hello ${name}, nice to meet you!`); // Возвращаем сообщение
 };
 
-export { greeting };
+const question = str => console.log(`Question: ${str}`);
+
+const even = () => {
+  console.log('Welcome to the Brain Games!'); // Приветствие
+  console.log('Answer "yes" if number even otherwise answer "no".'); // Rules
+  const name = readlineSync.question('May I have your name?'); // Спрашиваем имя
+  console.log(`Hello ${name}, nice to meet you!`); // Возвращаем сообщение
+
+  const correct = () => console.log('Correct!');
+
+  const unCorrect = (w, r) => {
+    console.log(`'${w}' is wrong answer ;(. Correct answer was '${r}'`);
+    console.log(`Let's try again, ${name}!`);
+  };
+
+  for (let i = 1; i < 4; i += 1) {
+    console.log(`${question(i)} `);
+    const answer = readlineSync.question('Your answer: ');
+    if (i % 2 === 0) {
+      if (answer === 'yes') {
+        correct();
+      } else {
+        unCorrect(answer, 'yes');
+        return;
+      }
+    } else {
+      if (answer === 'no') {
+        correct();
+      } else {
+        unCorrect(answer, 'no');
+        return;
+      }
+    }
+  }
+
+  console.log(`Congratulations, ${name}`);
+};
+
+export { greeting, even };
