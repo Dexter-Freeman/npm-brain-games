ce750eda56ac545954a8bcbb69dad97bc0420e40
Admin <F:\The Bat!\Data\Mail>
2018-02-13 11:04:46 +0300

    Fix eslint npm script, fix bage Travis CI

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { greeting } from '../index.js';  // Импортируем функцию greeting из index.js
+import { greeting } from '../index'; // Импортируем функцию greeting из index.js
 
-console.log('Welcome to the Brain Games!');  //Приветствие
-greeting(); //Делаем вызов функции greeting 
+console.log('Welcome to the Brain Games!'); // Приветствие
+greeting(); // Делаем вызов функции greeting
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import readlineSync from 'readline-sync'; // Импортируем readline-syn
 // var readlineSync = require('readline-sync');
 
 const greeting = () => {
-	const name = readlineSync.question('May I have your name?'); // Спрашиваем имя
-	return console.log('Hello ' + name + ', nice to meet you!'); // Возвращаем сообщение
+  const name = readlineSync.question('May I have your name?'); // Спрашиваем имя
+  return console.log('Hello ' + name + ', nice to meet you!'); // Возвращаем сообщение
 };
 // greeting();
 // exports.greeting = greeting;
 
-export { greeting };
+export default greeting;
