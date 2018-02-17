8608cce246f4cde9d5bcc5cd735719ad17d8d093
Admin <F:\The Bat!\Data\Mail>
2018-02-17 19:54:10 +0300

    Step 8, Game Progression. Done

--- src/games/balance.js
+++ src/games/balance.js
@@ -5,33 +5,17 @@ import { random, game } from '../index';
 export default function balnce() {
   const rules = 'Find the greatest common divisor of given numbers.'; // Rules
   const generateQuastionAndAnswer = () => { // Return pair question-answer
-  // Here we describe a function that generates a question-answer pair and returns it as a pair
-    const pair = cons(random(10000), random(10000)); // Variable for storing numbers used in the game
+  // Here we describe a function that generates a question-answer pair and
+  // returns it as a pair
+    const pair = cons(random(10000), random(10000)); // Variable for storing numbers
+    // used in the game
     const question = `${car(pair)}`; // Initialize veriable for question
     const isBalance = (n) => { // A function that checks if a number is balanced
-      if ((Number(n[n.length - 1]) - Number(n[0])) <= 1) { // If the difference between the last and first digits is less than or equal to 1
+      if ((Number(n[n.length - 1]) - Number(n[0])) <= 1) { // If the difference between the last
+        // and first digits is less than or equal to 1
         return true;
       }
       return false; // Otherwise
-    };
-    const bal = (num) => { // Gets an array
-      if (isBalance(num)) { // Checks if the number is balanced
-        return num.join(''); // If balanced, then returns that number as a string
-      } else { // Otherwise
-        const n1 = Number(num[0]) + 1; // Increases the first digit by 1
-        const n2 = Number(num[num.length - 1]) - 1; // Reduces the last digit by 1
-        if (num.length === 2) { // If the number consists of two digits
-          num.shift(); // Delete the first digit in the number
-          num.pop(); // Remove the last digit in the number
-          num.unshift(n1); // Replace the first digit in the number
-          num.push(n2); // Заменяем последнюю цифру в числе
-        } else {
-          num.shift(); // Delete the first digit in the number
-          num.pop(); // Remove the last digit in the number
-          num.unshift(n1); // Replace the first digit in the number
-          num.push(n2); // Заменяем последнюю цифру в числе
-        } return balanceEngine(num.join('')); // Balancing a new number
-      }
     };
     const sort = (num) => { // A function that sorts the numbers in the number in ascending order
       const unSortingString = num.split(''); // Get an array consisting of digits
@@ -42,9 +26,26 @@ export default function balnce() {
       if (num.length === 1) {
         return num; // If the number of length is 1 digit, then we return number
       }
+      const bal = (numer) => { // Gets an array
+        if (isBalance(numer)) { // Checks if the number is balanced
+          return numer.join(''); // If balanced, then returns that number as a string
+        } // Otherwise
+        const n1 = Number(numer[0]) + 1; // Increases the first digit by 1
+        const n2 = Number(numer[numer.length - 1]) - 1; // Reduces the last digit by 1
+        if (numer.length === 2) { // If the number consists of two digits
+          numer.shift(); // Delete the first digit in the number
+          numer.pop(); // Remove the last digit in the number
+          numer.unshift(n1); // Replace the first digit in the number
+          numer.push(n2); // Заменяем последнюю цифру в числе
+        } else {
+          numer.shift(); // Delete the first digit in the number
+          numer.pop(); // Remove the last digit in the number
+          numer.unshift(n1); // Replace the first digit in the number
+          numer.push(n2); // Заменяем последнюю цифру в числе
+        } return balanceEngine(numer.join('')); // Balancing a new number
+      };
       return bal(sort(num)); // Or pass on the sort and balancer
     };
-
     const answer = balanceEngine(String(question)); // Initialize veriable for answer
     return cons(question, answer); // Return pair question-answer
   };
---
+++ src/games/progression.js
@@ -0,0 +1,29 @@
+
+import { cons, car, cdr } from 'hexlet-pairs';
+import { random, game } from '../index';
+
+export default function progression() {
+  const rules = 'What number is missing in this progression?'; // Rules
+  const generateQuastionAndAnswer = () => { // Return pair question-answer
+  // Here we describe a function that generates a question-answer pair and
+  // returns it as a pair
+    const pair = cons(random(20), random(10)); // Variable for storing numbers
+    // used in the game
+    const progressionGenerator = (base, step) => { // Function to generate progression
+      const arr = []; // Create an empty array
+      arr.unshift(base); // The first element do base
+      for (let i = 0; i < 8; i++) {
+        arr.push(arr[i] + step); // Generate the following sequence element by adding a step
+      } return arr; // Return the resulting sequence as an array
+    };
+    const bone = random(9); // the element number of the array
+    const answer = arr => arr[bone]; // Answer - array element with an index of bone
+    const question = (arr) => {
+      arr.splice(bone, 1, '..'); // Replace the array element with index bone
+      return arr.join(' '); // Return the question string
+    };
+    // Return pair question-answer
+    return cons(question(progressionGenerator(car(pair), cdr(pair))), answer(progressionGenerator(car(pair), cdr(pair))));
+  };
+  game(rules, generateQuastionAndAnswer); // Run game
+}
